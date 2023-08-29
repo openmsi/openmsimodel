c5abb76ceb7c289148a1eeee90e5ae5e808e1ae3
arachid1 <alilerachidi@gmail.com>
2023-08-29 10:14:10 -0400

    testing docs

--- openmsimodel/utilities/typing.py
+++ openmsimodel/utilities/typing.py
@@ -45,43 +45,43 @@ SpecOrRunLiteral: TypeAlias = Literal["spec", "run", "both"]
 Attributes: TypeAlias = Union[Condition, Parameter, Property, PropertyAndConditions]
 
 
-class TagsDict(TypedDict):
-    """Define dictionary representation of hierarchical tags."""
+# class TagsDict(TypedDict):
+#     """Define dictionary representation of hierarchical tags."""
 
-    spec: dict[str, dict]
-    run: dict[str, dict]
-
-
-class FileLinksDict(TypedDict):
-    """Define dictionary containing file link strings for spec and run."""
-
-    spec: tuple[str, ...]
-    run: tuple[str, ...]
+#     spec: dict[str, dict]
+#     run: dict[str, dict]
 
 
-class ProcessDict(TypedDict):
-    """Define dictionary containing process string for the spec and run."""
+# class FileLinksDict(TypedDict):
+#     """Define dictionary containing file link strings for spec and run."""
 
-    spec: str
-    run: str
-
-
-class ValueAndOrigin(TypedDict):
-    """Attribute value and origin as a dictionary."""
-
-    value: dict[str, Any]
-    origin: Origin
+#     spec: tuple[str, ...]
+#     run: tuple[str, ...]
 
 
-class PropsAndCondsDict(TypedDict):
-    """Define dictionary output for material spec properties and conditions."""
+# class ProcessDict(TypedDict):
+#     """Define dictionary containing process string for the spec and run."""
 
-    property: ValueAndOrigin
-    conditions: dict[str, ValueAndOrigin]
+#     spec: str
+#     run: str
 
 
-class ValueOriginDict(TypedDict):
-    """Define dictionary output for process and measurement spec/run conditions/parameters."""
+# class ValueAndOrigin(TypedDict):
+#     """Attribute value and origin as a dictionary."""
 
-    spec: Optional[ValueAndOrigin]
-    run: Optional[ValueAndOrigin]
+#     value: dict[str, Any]
+#     origin: Origin
+
+
+# class PropsAndCondsDict(TypedDict):
+#     """Define dictionary output for material spec properties and conditions."""
+
+#     property: ValueAndOrigin
+#     conditions: dict[str, ValueAndOrigin]
+
+
+# class ValueOriginDict(TypedDict):
+#     """Define dictionary output for process and measurement spec/run conditions/parameters."""
+
+#     spec: Optional[ValueAndOrigin]
+#     run: Optional[ValueAndOrigin]
