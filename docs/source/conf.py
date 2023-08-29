f1e966f2a4e7bdae672ee09877c0709745e882f6
arachid1 <alilerachidi@gmail.com>
2023-08-29 09:21:48 -0400

    testing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,19 +9,20 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../openmsimodel"))
 
 
 # -- Project information -----------------------------------------------------
-project = 'openmsimodel'
-copyright = '2023, Ali Rachidi'
-author = 'Ali Rachidi'
+project = "openmsimodel"
+copyright = "2023, Ali Rachidi"
+author = "Ali Rachidi"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,22 +31,22 @@ release = '0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosectionlabel',
-    'sphinx.ext.intersphinx',
-    'sphinx_rtd_theme',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.intersphinx",
+    "sphinx_rtd_theme",
 ]
 
 # autodoc configs
 autodoc_default_options = {
-    'show-inheritance': None,
-    'members': None,
-    'member-order': 'bysource',
+    "show-inheritance": None,
+    "members": None,
+    "member-order": "bysource",
 }
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -53,22 +54,20 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 # Intersphinx mapping to Python 3 documentation
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 html_css_files = []
 
-html_theme_options = {
-    "sticky_navigation": False
-}
+html_theme_options = {"sticky_navigation": False}
