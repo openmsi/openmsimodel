d344dfa1e04edd5156d9c9aa9f160589af0a6c3b
arachid1 <alilerachidi@gmail.com>
2024-04-25 08:49:51 -0400

    extending live open graph

--- openmsimodel/graph/live_open_graph.py
+++ openmsimodel/graph/live_open_graph.py
@@ -11,6 +11,8 @@ from openmsimodel.structures.materials_sequence import MaterialsSequence
 from openmsimodel.graph.open_graph import OpenGraph
 import uuid
 
+# from gemd_schema import
+
 
 class LiveOpenGraph(FileSystemEventHandler):
     mapping = {}
@@ -64,19 +66,40 @@ def define_run(mapping):
     return materials_sequence.assets
 
 
+# def define_spec(file_extension, mapping, output):
+#     material_name = input("Enter material name: ")
+#     material = Material(name=material_name, template=MaterialTemplate(material_name))
+#     process_name = input("Enter process name: ")
+#     process = Process(name=process_name, template=ProcessTemplate(process_name))
+#     ingredient_name = input("Enter ingredient name: ")
+#     ingredient = Ingredient(name=ingredient_name)
+#     measurement_name = input("Enter measurement name: ")
+#     measurement = Measurement(
+#         name=measurement_name, template=MeasurementTemplate(measurement_name)
+#     )
+
+#     mapping[file_extension] = [material, process, [ingredient], [measurement]]
+#     return define_run(mapping[file_extension])
+
+
 def define_spec(file_extension, mapping, output):
-    material_name = input("Enter material name: ")
-    material = Material(name=material_name, template=MaterialTemplate(material_name))
-    process_name = input("Enter process name: ")
-    process = Process(name=process_name, template=ProcessTemplate(process_name))
-    ingredient_name = input("Enter ingredient name: ")
-    ingredient = Ingredient(name=ingredient_name)
-    measurement_name = input("Enter measurement name: ")
-    measurement = Measurement(
-        name=measurement_name, template=MeasurementTemplate(measurement_name)
-    )
-    mapping[file_extension] = [material, process, [ingredient], [measurement]]
-    return define_run(mapping[file_extension])
+    template = {}
+    for key, value in process_template_schema["properties"].items():
+        field_name = key
+        field_type = value.get("type")
+        field_description = value.get("description", "")
+
+        if field_type == "array":
+            template[field_name] = questionary.text(field_description).ask()
+        elif field_type == "string":
+            template[field_name] = questionary.text(field_description).ask()
+        # Add more conditions for other field types as needed...
+
+    # Validate the input based on the schema
+    validate(instance=template, schema=process_template_schema)
+
+    # Use the populated template object
+    print(template)
 
 
 if __name__ == "__main__":
